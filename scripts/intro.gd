6b2e12b43f7fb7d9f1ac2368a0ed61073399d2d3
Sebastian Molina <molinamolina@uninorte.edu.co>
2026-04-01 12:24:18 -0500

    recrear intro.gd

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,5 +1,11 @@
-# Solo lleva directamente a main.tscn por ahora.
-extends Control
+extends Node
 
-func _on_Pasar_pressed() -> void: 
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
